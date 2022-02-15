47d62830a61eadf22b69ff819b304c0c8b5aa849
swapnil <swapnilsharma98@live.com>
2022-02-16 00:45:11 +0600

    SQL update

--- dbtable.sql
+++ dbtable.sql
@@ -8,6 +8,9 @@ CourseTitle varchar(50) not null,
 CourseSession varchar(50) not null --year and semester combination
 );
 
+INSERT INTO Courses(CourseNo,CourseTitle,CourseSession) VALUES('CSE3100','SD','Spring-2020');
+INSERT INTO Courses(CourseNo,CourseTitle,CourseSession) VALUES('CSE3101','DB','Spring-2020');
+SELECT * FROM Courses WHERE CourseNo='CSE3100' AND CourseSession = 'Spring-2020';
 
 create table Students
 (
@@ -17,6 +20,11 @@ section varchar(10) not null,
 semester varchar(10) not null
 
 );
+
+INSERT INTO Students VALUES('190104143','Ineffable','C','3.1');
+INSERT INTO Students VALUES('190104140','Ashique','C','3.1');
+INSERT INTO Students VALUES('190104137','Swarna','C','3.1');
+INSERT INTO Students VALUES('190104116','Toufique','C','3.1');
 
 
 create table QuizMark(
@@ -36,8 +44,15 @@ create table StudentCourseJoin(
 	StudentID varchar(50),
 	constraint scj_student_fk foreign key(StudentID) references Students(StudentID),
 	constraint scj_course_fk foreign key(CourseID) references Courses(CourseID)
-	
 );
+
+INSERT INTO StudentCourseJoin VALUES(1001,'190104143');
+INSERT INTO StudentCourseJoin VALUES(1001,'190104140');
+INSERT INTO StudentCourseJoin VALUES(1002,'190104137');
+INSERT INTO StudentCourseJoin VALUES(1002,'190104116');
+
+TRUNCATE TABLE StudentCourseJoin;
+
 
 select * from StudentCourseJoin;
 
@@ -50,6 +65,10 @@ create table Attends(
 	constraint att_student_fk foreign key(StudentID) references Students(StudentID),
 	constraint att_course_fk foreign key(CourseID) references Courses(CourseID)
 )
+
+INSERT INTO Attends(StudentID,CourseID,ClassNo,ClassDate) VALUES('190104143',1001,1,GETDATE());
+INSERT INTO Attends(StudentID,CourseID,ClassNo,ClassDate) VALUES('190104143',1001,2,GETDATE());
+INSERT INTO Attends(StudentID,CourseID,ClassNo,ClassDate) VALUES('190104140',1001,1,GETDATE());
 
 
 create table Users
@@ -64,3 +83,22 @@ password varchar(50)
 insert into Users values('ineffable','swapnil','gg@ez.com','1234');
 
 drop table Users
+
+SELECT StudentCourseJoin.StudentID, Attends.ClassNo  
+FROM StudentCourseJoin
+LEFT JOIN ATTENDS
+ON StudentCourseJoin.CourseID = ATTENDS.courseID 
+AND Attends.StudentID = StudentCourseJoin.StudentID
+WHERE StudentCourseJoin.CourseID=1001;
+
+
+SELECT SelectedStudents.StudentID,
+CASE WHEN SelectedAttends.ClassNo IS NULL THEN 0
+ELSE 1
+END AS Present
+FROM (SELECT * FROM StudentCourseJoin WHERE CourseID = 1001) 
+AS SelectedStudents
+LEFT JOIN (SELECT * FROM Attends WHERE ClassNO = 2) AS SelectedAttends
+ON SelectedAttends.StudentID = SelectedStudents.StudentID;
+
+SELECT * from StudentCourseJoin;
